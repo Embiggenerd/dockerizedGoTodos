fec904f0dc3ae67f1e9492c998eadc45f887bd0e
igor <igoratakhanov@gmail.com>
2019-02-18 15:04:02 -0500

    Leave sse for different project. TODO: finish error handling, tests

--- goTodos/main.go
+++ goTodos/main.go
@@ -10,21 +10,4 @@ import (
 func main() {
 	models.Init()
 	routes.Init()
-	// var err string
-	// cacheBustedCss, err = utils.BustaCache("mainFloats.css", cacheBustedCss)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// fs := http.FileServer(http.Dir("public/"))
-	// http.Handle("/static/", http.StripPrefix("/static", fs))
-	// http.HandleFunc("/", authRequired(indexHandler))
-	// http.HandleFunc("/submit", authRequired(submitHandler))
-	// http.HandleFunc("/edit/", authRequired(editHandler))
-	// http.HandleFunc("/delete/", deleteHandler)
-	// http.HandleFunc("/register", registerUserHandler)
-	// http.HandleFunc("/login", loginUserHandler)
-	// http.HandleFunc("/logout", logoutUserHandler)
-	// http.HandleFunc("/oops", status500Handler)
-
-	// http.ListenAndServe(":8000", nil)
 }
