4285e90a0c91703454db4782af36c93b62164689
igor <embiggenerd@gmail.com>
2020-07-11 19:27:11 -0400

    added logs file

--- README.md
+++ README.md
@@ -9,18 +9,29 @@ request counter. Visit the app and check that the number has changed.
 
 ## In Detail
 
+### User Experience
 * Input some details to register. Only email and password are necessary. Check out error messages. 
 * Login, hit the submit button. You can edit, delete todos.
+
+### User Authentication
 * Users are authenticated by a hex token, not user ID.
-* A sessiosn table has a userID and hex values.
+* A session table has a userID and hex columns.
 * On login, a new random hex value is created, inserted into a new session, and old session is deleted.
-* To authenticate, middleware gets userid by looking up hex token in cookie in sessions table.
+* To authenticate, middleware gets userid by looking up hex token in cookie.
 * Hex token is refreshed every login for security, old sessions erased for space
 * This makes man in the middle attacks more difficult
-* Simple metrics middleware is implemented to count number of requests
+
+### Logging
+* Middleware logs request data to json file as well as stdout
+* Logs file is human readable, with newest at the top
+
+### Metrics
+* All standard pprof endpoints are available
+* /debug/vars will also show number of requests through middleware
 
 ## Demonstrates
 
-* Ability to read documentation and source code.
+* Docker, docker-compose
+* Basic sql, crud functionality
 * User registration/login
 * Metrics and observability
--- goTodos/logger/logger.go
+++ goTodos/logger/logger.go
@@ -1,11 +1,22 @@
 package logger
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"path/filepath"
 	"runtime/debug"
 	"time"
 )
+
+type requestLog struct {
+	Status   int
+	Method   string
+	Path     string
+	Duration time.Duration
+}
 
 type responseWriter struct {
 	http.ResponseWriter
@@ -23,7 +34,7 @@ func (rw *responseWriter) Status() int {
 	return rw.status
 }
 
-// WriteHeader turns the wroteHeader property true
+// WriteHeader turns the wroteHeader property true to prevent extra write
 func (rw *responseWriter) WriteHeader(code int) {
 	if rw.wroteHeader {
 		return
@@ -36,7 +47,8 @@ func (rw *responseWriter) WriteHeader(code int) {
 	return
 }
 
-// LoggingMiddleware wraps our router to get a handle on
+// LoggingMiddleware wraps our router to get a handle on requests. If a logs file
+// exists, it adds request log to json array. Else, it creates new file and logs request
 func LoggingMiddleware(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
@@ -59,7 +71,46 @@ func LoggingMiddleware(next http.Handler) http.Handler {
 			"path", r.URL.EscapedPath(),
 			"duration", time.Since(start),
 		)
+		request := requestLog{
+			Status:   wrapped.status,
+			Method:   r.Method,
+			Path:     r.URL.EscapedPath(),
+			Duration: time.Since(start),
+		}
+
+		logsFilepath := filepath.Join("logger", "logs.json")
+
+		var logsArr []interface{}
+
+		if fileExists(logsFilepath) {
+
+			logs, _ := ioutil.ReadFile(logsFilepath)
+
+			json.Unmarshal(logs, &logsArr)
+
+			logsArr = append(logsArr, request)
+
+			logsArrJSON, _ := json.MarshalIndent(&logsArr, "", " ")
+
+			ioutil.WriteFile(logsFilepath, logsArrJSON, 0644)
+
+		} else {
+
+			logsArr = append(logsArr, request)
+
+			logsArrJSON, _ := json.MarshalIndent(&logsArr, "", " ")
+
+			ioutil.WriteFile(logsFilepath, logsArrJSON, 0644)
+		}
 	}
 
 	return http.HandlerFunc(fn)
+}
+
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
 }
